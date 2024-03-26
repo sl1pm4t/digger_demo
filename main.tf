58a00c081d23e5a73f8af3abf71cf544b54aae7e
Matt Morrison <matt.morrison@ditto.live>
2024-03-26 14:05:23 +1300

    res2

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ resource "null_resource" "resource2" {
 resource "null_resource" "resource3" {
 }
 
-output "xx" {
+output "res1" {
    value = null_resource.resource1.id
+}
+
+output "res2" {
+   value = null_resource.resource2.id
 }
